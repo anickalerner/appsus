c528a67e73270effad39df7cfd1cdc5a4817963a
Anna Lerner <anicka.lerner@gmail.com>
2020-08-26 12:43:55 +0300

    Add storage service

---
+++ apps/mail/service/mail-service.js
@@ -0,0 +1,21 @@
+var mails = [
+    {
+        from: 'ebay',
+        subject: '✅ ORDER CONFIRMED: D\'Andrea Guitar Pick..',
+        body: 'Thanks for your purchase, Anna! Your order is confirmed. We’ll let you know when it’s on the way.In the meantime, we have more things you’ll love at unbeatable prices!'
+        , isRead: false, sentAt: 1551133930594
+    }
+    , {
+        from: 'Dropbox',
+        subject: 'You deleted 3034 files from Dropbox',
+        body: 'Hi Anna You recently deleted 3034 files from your Dropbox account.If you want these files back, you can still restore them until 16/9/2020.After that, they’ll be permanently deleted.'
+        , isRead: false, sentAt: 1551133930594
+    }
+    , {
+        from: 'Gett',
+        subject: 'Your Thursday night ride with Gett'
+        , body: 'Hi Anna, Thanks for using Gett! YOUR RIDE ID 1253858834'
+        , isRead: false, sentAt: 1551133930594
+    }
+]
+
