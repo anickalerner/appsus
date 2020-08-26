92d16440ce68f7acbcb6256cb6f097dfd2691503
Anna Lerner <anicka.lerner@gmail.com>
2020-08-26 16:48:51 +0300

    Fixed clasName in Icons

--- cmps/Icons.jsx
+++ cmps/Icons.jsx
@@ -3,57 +3,57 @@ function getFontSize(size){
 }
 //Mail
 export function TrashBinIcon({size}){
-    return (<i class="fas fa-trash" style={getFontSize(size)}></i>)
+    return (<i className="fas fa-trash" style={getFontSize(size)}></i>)
 }
 export function InboxIcon({ size }) {
-    return (<i class="fas fa-inbox" style={getFontSize(size)}></i>)
+    return (<i className="fas fa-inbox" style={getFontSize(size)}></i>)
 }
 export function StarIcon({ size }) {
-    return (<i class="fas fa-star" style={getFontSize(size)}></i>)
+    return (<i className="fas fa-star" style={getFontSize(size)}></i>)
 }
 export function SentIcon({ size }) {
-    return (<i class="fas fa-share-square" style={getFontSize(size)}></i>)
+    return (<i className="fas fa-share-square" style={getFontSize(size)}></i>)
 }
 export function DraftIcon({ size }) {
-    return (<i class="fas fa-firstdraft" style={getFontSize(size)}></i>)
+    return (<i className="fas fa-firstdraft" style={getFontSize(size)}></i>)
 }
 export function PlusIcon({ size }) {
-    return (<i class="fas fa-plus" style={getFontSize(size)}></i>)
+    return (<i className="fas fa-plus" style={getFontSize(size)}></i>)
 }
 export function EnvelopeOpenIcon({ size }) {
-    return (<i class="fas fa-envelope-open" style={getFontSize(size)}></i>)
+    return (<i className="fas fa-envelope-open" style={getFontSize(size)}></i>)
 }
 export function EnvelopeIcon({ size }) {
-    return (<i class="fas fa-envelope" style={getFontSize(size)}></i>)
+    return (<i className="fas fa-envelope" style={getFontSize(size)}></i>)
 }
 
 //Keep
 export function PaletteIcon({ size }) {
-    return (<i class="fas fa-palette" style={getFontSize(size)}></i>)
+    return (<i className="fas fa-palette" style={getFontSize(size)}></i>)
 }
 export function ImageIcon({ size }) {
-    return (<i class="fas fa-image" style={getFontSize(size)}></i>)
+    return (<i className="fas fa-image" style={getFontSize(size)}></i>)
 }
 export function PinIcon({ size }) {
-    return (<i class="fas fa-thumbtack" style={getFontSize(size)}></i>)
+    return (<i className="fas fa-thumbtack" style={getFontSize(size)}></i>)
 }
 export function YoutubeIcon({ size }) {
-    return (<i class="fas fa-youtube" style={getFontSize(size)}></i>)
+    return (<i className="fas fa-youtube" style={getFontSize(size)}></i>)
 }
 export function SoundIcon({ size }) {
-    return (<i class="fas fa-volume-up" style={getFontSize(size)}></i>)
+    return (<i className="fas fa-volume-up" style={getFontSize(size)}></i>)
 }
 export function FontIcon({ size }) {
-    return (<i class="fas fa-font" style={getFontSize(size)}></i>)
+    return (<i className="fas fa-font" style={getFontSize(size)}></i>)
 }
 export function EditIcon({ size }) {
-    return (<i class="fas fa-edit" style={getFontSize(size)}></i>)
+    return (<i className="fas fa-edit" style={getFontSize(size)}></i>)
 }
 export function CopyIcon({ size }) {
-    return (<i class="fas fa-copy" style={getFontSize(size)}></i>)
+    return (<i className="fas fa-copy" style={getFontSize(size)}></i>)
 }
 export function CheckIcon({ size }) {
-    return (<i class="fas fa-check" style={getFontSize(size)}></i>)
+    return (<i className="fas fa-check" style={getFontSize(size)}></i>)
 }
 
 
