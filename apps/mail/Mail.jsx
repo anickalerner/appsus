d9252b4ab674667719f7bde09218dd041d738d61
Anna Lerner <anicka.lerner@gmail.com>
2020-08-27 14:20:17 +0300

    Mail Filtering

--- apps/mail/Mail.jsx
+++ apps/mail/Mail.jsx
@@ -1,6 +1,7 @@
-import { mailService } from 'service/mail-service.js';
-import { MailList } from 'cmps/MailList.jsx';
-import { MailMenu } from 'cmps/MailMenu.jsx';
+import { mailService } from './service/mail-service.js';
+import { MailList } from './cmps/MailList.jsx';
+import { MailMenu } from './cmps/MailMenu.jsx';
+import { NewMail } from './cmps/NewMail.jsx';
 
 export default class Mail extends React.Component {
 
@@ -9,17 +10,43 @@ export default class Mail extends React.Component {
     };
 
     componentDidMount() {
-        console.log(this.props.match.params.filter);
-        // componentDidMount = () => {
-        //     const bookId = this.props.match.params.filter;
-        //     bookService.getBookById(bookId)
-        //         .then(book => this.setState({ book }));
-        // }
         this.loadMails();
     }
 
-    loadMails() {
-        mailService.query().then(mails => this.setState({ mails }));
+    componentDidUpdate(prevProps, prevState) {
+        if (prevProps.match.params.filter !== this.props.match.params.filter) {
+            this.loadMails();
+        }
+    }
+
+    getFilter = () =>{
+        var filter = this.props.match.params.filter;
+        return (!filter) ? 'all' : filter;
+    }
+
+    loadMails = (_filter) => {
+        var filter = (_filter) ? _filter : this.getFilter();
+        mailService.query(filter).then(mails => this.setState({ mails }));
+    }
+
+    mailStarToggle = (mailToUpdateId) =>{
+        mailService.getMailIndexById(mailToUpdateId).then(mailInd=>{
+            var mailToUpdate = this.state.mails[mailInd];
+            mailToUpdate.isStarred = !mailToUpdate.isStarred;
+            mailService.updateMail(mailInd, mailToUpdate).then(()=>this.loadMails());
+        });
+    }
+
+    onCompose = () => {
+        this.setState({composing: true});
+    }
+
+    saveDraft = () => {
+        this.setState({ composing: false });
+    }
+
+    onSendMail = (formData) => {
+
     }
 
     render() {
@@ -27,8 +54,9 @@ export default class Mail extends React.Component {
             <section className="mail-container">
                 <h2>Mail</h2>
                 <div className="mail-wrapper">
-                <MailMenu/>
-                <MailList mails={this.state.mails} />
+                    <MailMenu onCompose={this.onCompose}/>
+                    <MailList mails={this.state.mails} mailStarToggle={this.mailStarToggle}/>
+                    {this.state.composing && <NewMail onClose={this.saveDraft} onSend={this.onSendMail}/>}
                 </div>                
             </section>
         )
